095b91ac2ad6b0fb4cfd6c4c7c4967f442186e2d
Alexandra Bocirnea <alexandra.bocirnea@cerner.com>
2022-09-15 20:28:23 +0300

    ExistingVolunteerHello

--- Proiect C#/bucuriaDarului/PontajVoluntarExistentcs.Designer.cs
+++ Proiect C#/bucuriaDarului/PontajVoluntarExistentcs.Designer.cs
@@ -1,5 +1,6 @@
 ﻿namespace bucuriaDarului
 {
+
     partial class PontajVoluntarExistentcs
     {
         /// <summary>
@@ -28,12 +29,305 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dateTimePickerIntrare = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerIesire = new System.Windows.Forms.DateTimePicker();
+            this.OradeIntrareLabel = new System.Windows.Forms.Label();
+            this.OradeIesireLabel = new System.Windows.Forms.Label();
+            this.NumardeOreLabel = new System.Windows.Forms.Label();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.numeleVoluntaruluiLabel = new System.Windows.Forms.Label();
+            this.maximOreLabel = new System.Windows.Forms.Label();
+            this.tipDeActivitateLabel = new System.Windows.Forms.Label();
+            this.checkBoxBucatarie = new System.Windows.Forms.CheckBox();
+            this.checkBoxPachete = new System.Windows.Forms.CheckBox();
+            this.checkBoxPacheteSate = new System.Windows.Forms.CheckBox();
+            this.checkBoxPacheteFamilii = new System.Windows.Forms.CheckBox();
+            this.checkBoxTranspPachete = new System.Windows.Forms.CheckBox();
+            this.checkBoxEtichetat = new System.Windows.Forms.CheckBox();
+            this.checkBoxCampanii = new System.Windows.Forms.CheckBox();
+            this.checkBoxEducatie = new System.Windows.Forms.CheckBox();
+            this.checkBoxEducatieLectii = new System.Windows.Forms.CheckBox();
+            this.checkBoxEducatieNonconf = new System.Windows.Forms.CheckBox();
+            this.DataLabel = new System.Windows.Forms.Label();
+            this.dataCurentaLabel = new System.Windows.Forms.Label();
+            this.oreDeActivitateLabel = new System.Windows.Forms.Label();
+            this.buttonConfirmPontajVExistent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerIntrare
+            // 
+            this.dateTimePickerIntrare.Location = new System.Drawing.Point(188, 125);
+            this.dateTimePickerIntrare.Name = "dateTimePickerIntrare";
+            this.dateTimePickerIntrare.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePickerIntrare.TabIndex = 0;
+            // 
+            // dateTimePickerIesire
+            // 
+            this.dateTimePickerIesire.Location = new System.Drawing.Point(188, 170);
+            this.dateTimePickerIesire.Name = "dateTimePickerIesire";
+            this.dateTimePickerIesire.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePickerIesire.TabIndex = 1;
+            // 
+            // OradeIntrareLabel
+            // 
+            this.OradeIntrareLabel.AutoSize = true;
+            this.OradeIntrareLabel.Location = new System.Drawing.Point(72, 125);
+            this.OradeIntrareLabel.Name = "OradeIntrareLabel";
+            this.OradeIntrareLabel.Size = new System.Drawing.Size(85, 15);
+            this.OradeIntrareLabel.TabIndex = 2;
+            this.OradeIntrareLabel.Text = "Ora de Intrare :";
+            this.OradeIntrareLabel.Click += new System.EventHandler(this.OradeIntrare_Click);
+            // 
+            // OradeIesireLabel
+            // 
+            this.OradeIesireLabel.AutoSize = true;
+            this.OradeIesireLabel.Location = new System.Drawing.Point(72, 176);
+            this.OradeIesireLabel.Name = "OradeIesireLabel";
+            this.OradeIesireLabel.Size = new System.Drawing.Size(78, 15);
+            this.OradeIesireLabel.TabIndex = 3;
+            this.OradeIesireLabel.Text = "Ora de Iesire :";
+            // 
+            // NumardeOreLabel
+            // 
+            this.NumardeOreLabel.AutoSize = true;
+            this.NumardeOreLabel.Location = new System.Drawing.Point(72, 240);
+            this.NumardeOreLabel.Name = "NumardeOreLabel";
+            this.NumardeOreLabel.Size = new System.Drawing.Size(88, 15);
+            this.NumardeOreLabel.TabIndex = 4;
+            this.NumardeOreLabel.Text = "Numar de Ore :";
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Location = new System.Drawing.Point(72, 52);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(52, 15);
+            this.welcomeLabel.TabIndex = 6;
+            this.welcomeLabel.Text = "Salutare,";
+            // 
+            // numeleVoluntaruluiLabel
+            // 
+            this.numeleVoluntaruluiLabel.AutoSize = true;
+            this.numeleVoluntaruluiLabel.Location = new System.Drawing.Point(135, 52);
+            this.numeleVoluntaruluiLabel.Name = "numeleVoluntaruluiLabel";
+            this.numeleVoluntaruluiLabel.Size = new System.Drawing.Size(106, 15);
+            this.numeleVoluntaruluiLabel.TabIndex = 7;
+            this.numeleVoluntaruluiLabel.Text = "insereazanumele+!";
+            // 
+            // maximOreLabel
+            // 
+            this.maximOreLabel.AutoSize = true;
+            this.maximOreLabel.Location = new System.Drawing.Point(291, 240);
+            this.maximOreLabel.Name = "maximOreLabel";
+            this.maximOreLabel.Size = new System.Drawing.Size(122, 15);
+            this.maximOreLabel.TabIndex = 8;
+            this.maximOreLabel.Text = "* Maxim 8 ore admise";
+            // 
+            // tipDeActivitateLabel
+            // 
+            this.tipDeActivitateLabel.AutoSize = true;
+            this.tipDeActivitateLabel.Location = new System.Drawing.Point(72, 294);
+            this.tipDeActivitateLabel.Name = "tipDeActivitateLabel";
+            this.tipDeActivitateLabel.Size = new System.Drawing.Size(102, 15);
+            this.tipDeActivitateLabel.TabIndex = 9;
+            this.tipDeActivitateLabel.Text = "Tipul de Activitate";
+            // 
+            // checkBoxBucatarie
+            // 
+            this.checkBoxBucatarie.AutoSize = true;
+            this.checkBoxBucatarie.Location = new System.Drawing.Point(99, 325);
+            this.checkBoxBucatarie.Name = "checkBoxBucatarie";
+            this.checkBoxBucatarie.Size = new System.Drawing.Size(75, 19);
+            this.checkBoxBucatarie.TabIndex = 10;
+            this.checkBoxBucatarie.Text = "Bucatarie";
+            this.checkBoxBucatarie.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPachete
+            // 
+            this.checkBoxPachete.AutoSize = true;
+            this.checkBoxPachete.Location = new System.Drawing.Point(99, 352);
+            this.checkBoxPachete.Name = "checkBoxPachete";
+            this.checkBoxPachete.Size = new System.Drawing.Size(68, 19);
+            this.checkBoxPachete.TabIndex = 11;
+            this.checkBoxPachete.Text = "Pachete";
+            this.checkBoxPachete.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPacheteSate
+            // 
+            this.checkBoxPacheteSate.AutoSize = true;
+            this.checkBoxPacheteSate.Location = new System.Drawing.Point(99, 377);
+            this.checkBoxPacheteSate.Name = "checkBoxPacheteSate";
+            this.checkBoxPacheteSate.Size = new System.Drawing.Size(93, 19);
+            this.checkBoxPacheteSate.TabIndex = 12;
+            this.checkBoxPacheteSate.Text = "Pachete Sate";
+            this.checkBoxPacheteSate.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPacheteFamilii
+            // 
+            this.checkBoxPacheteFamilii.AutoSize = true;
+            this.checkBoxPacheteFamilii.Location = new System.Drawing.Point(99, 402);
+            this.checkBoxPacheteFamilii.Name = "checkBoxPacheteFamilii";
+            this.checkBoxPacheteFamilii.Size = new System.Drawing.Size(106, 19);
+            this.checkBoxPacheteFamilii.TabIndex = 13;
+            this.checkBoxPacheteFamilii.Text = "Pachete Familii";
+            this.checkBoxPacheteFamilii.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTranspPachete
+            // 
+            this.checkBoxTranspPachete.AutoSize = true;
+            this.checkBoxTranspPachete.Location = new System.Drawing.Point(99, 427);
+            this.checkBoxTranspPachete.Name = "checkBoxTranspPachete";
+            this.checkBoxTranspPachete.Size = new System.Drawing.Size(158, 19);
+            this.checkBoxTranspPachete.TabIndex = 14;
+            this.checkBoxTranspPachete.Text = "Transport Pachete Familii";
+            this.checkBoxTranspPachete.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEtichetat
+            // 
+            this.checkBoxEtichetat.AutoSize = true;
+            this.checkBoxEtichetat.Location = new System.Drawing.Point(350, 320);
+            this.checkBoxEtichetat.Name = "checkBoxEtichetat";
+            this.checkBoxEtichetat.Size = new System.Drawing.Size(72, 19);
+            this.checkBoxEtichetat.TabIndex = 15;
+            this.checkBoxEtichetat.Text = "Etichetat";
+            this.checkBoxEtichetat.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCampanii
+            // 
+            this.checkBoxCampanii.AutoSize = true;
+            this.checkBoxCampanii.Location = new System.Drawing.Point(350, 345);
+            this.checkBoxCampanii.Name = "checkBoxCampanii";
+            this.checkBoxCampanii.Size = new System.Drawing.Size(77, 19);
+            this.checkBoxCampanii.TabIndex = 16;
+            this.checkBoxCampanii.Text = "Campanii";
+            this.checkBoxCampanii.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEducatie
+            // 
+            this.checkBoxEducatie.AutoSize = true;
+            this.checkBoxEducatie.Location = new System.Drawing.Point(350, 370);
+            this.checkBoxEducatie.Name = "checkBoxEducatie";
+            this.checkBoxEducatie.Size = new System.Drawing.Size(71, 19);
+            this.checkBoxEducatie.TabIndex = 17;
+            this.checkBoxEducatie.Text = "Educatie";
+            this.checkBoxEducatie.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEducatieLectii
+            // 
+            this.checkBoxEducatieLectii.AutoSize = true;
+            this.checkBoxEducatieLectii.Location = new System.Drawing.Point(350, 395);
+            this.checkBoxEducatieLectii.Name = "checkBoxEducatieLectii";
+            this.checkBoxEducatieLectii.Size = new System.Drawing.Size(133, 19);
+            this.checkBoxEducatieLectii.TabIndex = 18;
+            this.checkBoxEducatieLectii.Text = "Educatie Lectii Copii";
+            this.checkBoxEducatieLectii.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEducatieNonconf
+            // 
+            this.checkBoxEducatieNonconf.AutoSize = true;
+            this.checkBoxEducatieNonconf.Location = new System.Drawing.Point(350, 420);
+            this.checkBoxEducatieNonconf.Name = "checkBoxEducatieNonconf";
+            this.checkBoxEducatieNonconf.Size = new System.Drawing.Size(188, 19);
+            this.checkBoxEducatieNonconf.TabIndex = 19;
+            this.checkBoxEducatieNonconf.Text = "Educatie Activitati Nonformale";
+            this.checkBoxEducatieNonconf.UseVisualStyleBackColor = true;
+            // 
+            // DataLabel
+            // 
+            this.DataLabel.AutoSize = true;
+            this.DataLabel.Location = new System.Drawing.Point(72, 87);
+            this.DataLabel.Name = "DataLabel";
+            this.DataLabel.Size = new System.Drawing.Size(31, 15);
+            this.DataLabel.TabIndex = 20;
+            this.DataLabel.Text = "Data";
+            // 
+            // dataCurentaLabel
+            // 
+            this.dataCurentaLabel.AutoSize = true;
+            this.dataCurentaLabel.Location = new System.Drawing.Point(129, 89);
+            this.dataCurentaLabel.Name = "dataCurentaLabel";
+            this.dataCurentaLabel.Size = new System.Drawing.Size(120, 15);
+            this.dataCurentaLabel.TabIndex = 21;
+            this.dataCurentaLabel.Text = "insereazadataCurenta";
+            // 
+            // oreDeActivitateLabel
+            // 
+            this.oreDeActivitateLabel.AutoSize = true;
+            this.oreDeActivitateLabel.Location = new System.Drawing.Point(166, 240);
+            this.oreDeActivitateLabel.Name = "oreDeActivitateLabel";
+            this.oreDeActivitateLabel.Size = new System.Drawing.Size(119, 15);
+            this.oreDeActivitateLabel.TabIndex = 22;
+            this.oreDeActivitateLabel.Text = "insertOreDeActivitate";
+            // 
+            // buttonConfirmPontajVExistent
+            // 
+            this.buttonConfirmPontajVExistent.Location = new System.Drawing.Point(352, 490);
+            this.buttonConfirmPontajVExistent.Name = "buttonConfirmPontajVExistent";
+            this.buttonConfirmPontajVExistent.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirmPontajVExistent.TabIndex = 23;
+            this.buttonConfirmPontajVExistent.Text = "Confirma";
+            this.buttonConfirmPontajVExistent.UseVisualStyleBackColor = true;
+            this.buttonConfirmPontajVExistent.Click += new System.EventHandler(this.buttonConfirmPontajVExistent_Click);
+            // 
+            // PontajVoluntarExistentcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 525);
+            this.Controls.Add(this.buttonConfirmPontajVExistent);
+            this.Controls.Add(this.oreDeActivitateLabel);
+            this.Controls.Add(this.dataCurentaLabel);
+            this.Controls.Add(this.DataLabel);
+            this.Controls.Add(this.checkBoxEducatieNonconf);
+            this.Controls.Add(this.checkBoxEducatieLectii);
+            this.Controls.Add(this.checkBoxEducatie);
+            this.Controls.Add(this.checkBoxCampanii);
+            this.Controls.Add(this.checkBoxEtichetat);
+            this.Controls.Add(this.checkBoxTranspPachete);
+            this.Controls.Add(this.checkBoxPacheteFamilii);
+            this.Controls.Add(this.checkBoxPacheteSate);
+            this.Controls.Add(this.checkBoxPachete);
+            this.Controls.Add(this.checkBoxBucatarie);
+            this.Controls.Add(this.tipDeActivitateLabel);
+            this.Controls.Add(this.maximOreLabel);
+            this.Controls.Add(this.numeleVoluntaruluiLabel);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.NumardeOreLabel);
+            this.Controls.Add(this.OradeIesireLabel);
+            this.Controls.Add(this.OradeIntrareLabel);
+            this.Controls.Add(this.dateTimePickerIesire);
+            this.Controls.Add(this.dateTimePickerIntrare);
+            this.Name = "PontajVoluntarExistentcs";
             this.Text = "PontajVoluntarExistentcs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DateTimePicker dateTimePickerIntrare;
+        private DateTimePicker dateTimePickerIesire;
+        private Label OradeIntrareLabel;
+        private Label OradeIesireLabel;
+        private Label NumardeOreLabel;
+        private Label welcomeLabel;
+        private Label numeleVoluntaruluiLabel;
+        private Label maximOreLabel;
+        private Label tipDeActivitateLabel;
+        private CheckBox checkBoxBucatarie;
+        private CheckBox checkBoxPachete;
+        private CheckBox checkBoxPacheteSate;
+        private CheckBox checkBoxPacheteFamilii;
+        private CheckBox checkBoxTranspPachete;
+        private CheckBox checkBoxEtichetat;
+        private CheckBox checkBoxCampanii;
+        private CheckBox checkBoxEducatie;
+        private CheckBox checkBoxEducatieLectii;
+        private CheckBox checkBoxEducatieNonconf;
+        private Label DataLabel;
+        private Label dataCurentaLabel;
+        private Label oreDeActivitateLabel;
+        private Button buttonConfirmPontajVExistent;
     }
 }
--- Proiect C#/bucuriaDarului/PontajVoluntarExistentcs.cs
+++ Proiect C#/bucuriaDarului/PontajVoluntarExistentcs.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,85 @@ namespace bucuriaDarului
 {
     public partial class PontajVoluntarExistentcs : Form
     {
+        
+        //char numarTelefonVoluntar;
+
         public PontajVoluntarExistentcs()
         {
             InitializeComponent();
+            //numarTelefonVoluntar = telefonVoluntar;
+            numeleVoluntaruluiLabel.Text = getVolunteerNameFromDB();
+        }
+
+        private void OradeIntrare_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Get name
+
+
+        private void buttonConfirmPontajVExistent_Click(object sender, EventArgs e)
+        {
+            //if true
+            SendExistingVolunteerData();
+            //if false
+             //MessageBox
+        }
+
+
+
+        private static string getVolunteerNameFromDB()
+        {
+            string volunteerName = "";
+            string queryString = "SELECT [FirstName] FROM [dbo].[Volunteers] WHERE [PhoneNumber]=0751049326;";
+
+            using (SqlConnection cnn = SingletonDB.GetDBConnection())
+            {
+                SqlCommand command =
+                    new SqlCommand(queryString, cnn);
+                cnn.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    volunteerName =  ReadSingleRow((IDataRecord)reader);
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+            }
+
+            return volunteerName;
+        }
+        private static string ReadSingleRow(IDataRecord dataRecord)
+        {
+            String resultVolName = (String.Format("{0}", dataRecord[0]));
+            return resultVolName;
+        }
+
+        static int SendExistingVolunteerData()
+        {
+            try
+            {
+                SqlConnection cnn = SingletonDB.GetDBConnection();
+                SingletonDB.OpenDatabaseConnection();
+                using (SqlCommand cmd = new("dbo.InsertPontaj", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.Add("@Prenume", SqlDbType.VarChar).Value = userNameBox.Text;
+                    cmd.Dispose();
+                }
+                SingletonDB.CloseDatabaseConnection();
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                return -1;
+            }
         }
     }
 }
